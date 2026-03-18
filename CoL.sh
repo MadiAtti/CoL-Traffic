d1579c6d8cc30ef592d7f40051b229203e2091b8
Madarász Attila <madiati@dhcp-955.q.wlan.net.bme.hu>
2026-03-18 10:59:04 +0100

    feat: add old code

---
+++ CoL.sh
@@ -0,0 +1,5 @@
+python3 CoL_NetworkTraffic.py --seed 5
+python3 CoL_NetworkTraffic.py --seed 6
+python3 CoL_NetworkTraffic.py --seed 7
+python3 CoL_NetworkTraffic.py --seed 8
+python3 CoL_NetworkTraffic.py --seed 9
